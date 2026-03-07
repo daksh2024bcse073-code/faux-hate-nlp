5086621b0c097a9a9b949212172c3a3706726e3e
daksh2024bcse073-code <daksh_2024bcse073@nitsri.ac.in>
2026-03-07 16:07:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,68 +3,43 @@ import torch
 import sys
 import os
 
-# Add src folder to path
 sys.path.append(os.path.join(os.path.dirname(__file__), "src"))
 
-# from train_roberta import train_roberta
-# from train_xlmroberta import train_xlmroberta
-# from train_deberta import train_deberta
+from train_roberta import train_roberta
+from train_xlmroberta import train_xlmroberta
+from train_deberta import train_deberta
 from src.rag.train_bge_llama3 import train_bge_llama3
-    
 
 
 def main():
+
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--model", type=str, required=True,
-                        choices=["roberta", "xlmroberta", "deberta"])
+    parser.add_argument(
+        "--model",
+        type=str,
+        choices=["roberta", "xlmroberta", "deberta", "bge_llama3"],
+        required=True
+    )
+
     parser.add_argument("--epochs", type=int, default=3)
     parser.add_argument("--batch_size", type=int, default=16)
-    parser.add_argument("--lr", type=float, default=2e-5)
+    parser.add_argument("--lr", type=float, default=1e-5)
 
     args = parser.parse_args()
 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-    print("=" * 40)
-    print("Using device:", device)
-    print("=" * 40)
-
     if args.model == "roberta":
-        train_roberta(
-            epochs=args.epochs,
-            batch_size=args.batch_size,
-            lr=args.lr,
-            device=device
-        )
+        train_roberta(args.epochs, args.batch_size, args.lr)
 
     elif args.model == "xlmroberta":
-        train_xlmroberta(
-            epochs=args.epochs,
-            batch_size=args.batch_size,
-            lr=args.lr,
-            device=device
-        )
+        train_xlmroberta(args.epochs, args.batch_size, args.lr)
 
     elif args.model == "deberta":
-        train_deberta(
-            epochs=args.epochs,
-            batch_size=args.batch_size,
-            lr=args.lr,
-            device=device
-        )
+        train_deberta(args.epochs, args.batch_size, args.lr)
 
-    else:
-        raise NotImplementedError(f"Model {args.model} not implemented yet!")
+    elif args.model == "bge_llama3":
+        train_bge_llama3()
 
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
-
